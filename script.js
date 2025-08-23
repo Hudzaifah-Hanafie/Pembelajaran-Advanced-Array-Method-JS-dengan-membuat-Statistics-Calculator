5bd70bf458255ae0a5d78e6dd1e0a8b750efa420
Hudzaifah-Hanafie <hanafiyaahmad@gmail.com>
2025-08-23 22:25:38 +0700

    menambahkan return false pada form di index untuk mencegah page reload, menyederhanakan getMean() dan menambahkan fungsi getMedian() untuk mendapatkan median dari nilai yg di inputkan

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const getMean = (array) => {
-    const sum = array.reduce((acc, num) => acc + num, 0);
-    return sum / array.length;
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const getMedian = (array) => {
+  const sorted = array.slice().sort((a, b) => a - b);
+    const mid = Math.floor(sorted.length / 2);
+    return sorted.length % 2 !== 0 ? sorted[mid] : (sorted[mid - 1] + sorted[mid]) / 2;
 };
 
 const calculate = () => {
-    const value = document.querySelector('#numbers').value;
-    const array = value.split(/,\s*/g);
-    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+  const mean = getMean(numbers);
+  document.querySelector("#mean").textContent = mean;
 };
